04b86313d61c5152b0e3891386a7838b63290a93
mw742 <58187552+mw742@users.noreply.github.com>
2023-09-08 15:01:02 +0100

    Delete desktop/Case_study directory

--- desktop/Case_study/scripts/sdftomae.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRNAME=$(dirname "$0")
-echo "The script is located in: $DIRNAME"
-cd "$DIRNAME"
-for file in *H.sdf; do
-    base_name="${file%.*}"; # This strips the extension from the file name
-    /shared/shared/schrodinger/2022-1/utilities/structconvert "$file" "$base_name.mae";
-done
