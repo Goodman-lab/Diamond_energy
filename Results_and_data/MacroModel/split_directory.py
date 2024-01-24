2d3784e8a56d3beb80b2d738f47321a107a93f5a
mw742 <58187552+mw742@users.noreply.github.com>
2024-01-24 21:12:43 +0000

    Delete Results_and_data/MacroModel directory

--- Results_and_data/MacroModel/split_directory.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8cace023cd10e0b05c42f0cf11f474988599ddaf85361e86da1d0baab89040e7
-size 27074
--- Results_and_data/MacroModel/split_directory_mae.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bdd393f46c7105d4e7609b8b9a93f53f82159248e4435ddc627b51d16e044b93
-size 14592
