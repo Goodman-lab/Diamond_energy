2123cdce0a6596252f3658a58978f5aaff37f04d
mw742 <58187552+mw742@users.noreply.github.com>
2024-01-24 21:12:52 +0000

    Delete Results_and_data/scripts directory

--- Results_and_data/scripts/generate_mol.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa1661a59293a43485de66b8ad63e364a2894141f1456c51c3ce59233c53dc8a
-size 18113
--- Results_and_data/scripts/maestro_mol.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5ba8fbd1907f28cb842112011efa44f5c945c7c1579f4e3e7da43f54fc16740d
-size 26276
